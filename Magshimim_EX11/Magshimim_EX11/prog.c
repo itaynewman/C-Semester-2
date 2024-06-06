d59897b409bd3d8d3f1a6c6a625c58fe8ae5a1ae
Itay <itaynewman@gmial.com>
2024-06-06 19:36:18 +0300

    new

---
+++ Magshimim_EX11/Magshimim_EX11/prog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void printPerfectSquares(int n);
+
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    getchar();  // Wait for Enter after input
+
+    printPerfectSquares(n);
+
+    printf("Press Enter to exit...");
+    getchar();  // Wait for Enter before exit
+
+    return 0;
+}
+
+void printPerfectSquares(int n) {
+    int i = 1;
+    while (i * i <= n) {
+        printf("%d\n", i * i);
+        i++;
+    }
+}
