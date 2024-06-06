45d15b38bee1e15e46fa61e792a13b7137ca6d8c
Itay <itaynewman@gmial.com>
2024-06-06 19:59:10 +0300

    better comments

--- Magshimim_EX11/Magshimim_EX11/prog.c
+++ Magshimim_EX11/Magshimim_EX11/prog.c
@@ -7,17 +7,17 @@ int main()
     int n;
     printf("Enter a positive integer: ");
     scanf("%d", &n);
-    getchar();  // Wait for Enter after input
+    getchar();
 
     printPerfectSquares(n);
 
     printf("Press Enter to exit...");
-    getchar();  // Wait for Enter before exit
-
+    
+    getchar(); 
     return 0;
 }
 
-void printPerfectSquares(int n) 
+void printPerfectSquares(int n)//O(sqrt(n)) runtime complexities
 {
     int i = 1;
     while (i * i <= n) 
