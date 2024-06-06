af2ecef6fcad8c09a46cec310e4f9dc685d8d310
Itay <itaynewman@gmial.com>
2024-06-06 19:22:32 +0300

    first q of week 11

---
+++ Magshimim_EX10]/Magshimim_EX10]/prog.c
@@ -0,0 +1,40 @@
+/*********************************
+* Class: MAGSHIMIM C2			 *
+* Week:                			 *
+* Name:                          *
+* Credits:                       *
+**********************************/
+
+#include <stdio.h>
+#include <math.h>
+
+void print_perfect_squares(int n);
+
+int main() 
+{
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    getchar();  // Wait for Enter after input
+
+    print_perfect_squares(n);
+
+    printf("Press Enter to exit...");
+    getchar();  // Wait for Enter before exit
+
+    return 0;
+}
+
+
+//description: This function prints all the perfect squares up to a given number.
+//input: The number up to which perfect squares will be printed.
+//output: Void
+void print_perfect_squares(int n) 
+{
+    int i = 1;
+    while (i * i <= n) 
+    {
+        printf("%d\n", i * i);
+        i++;
+    }
+}
