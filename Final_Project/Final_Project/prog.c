bcd3b5d9ea30e151515c5057654604e64aa72840
Itay <itaynewman@gmial.com>
2024-06-19 18:24:24 +0300

    fix

--- Final_Project/Final_Project/prog.c
+++ Final_Project/Final_Project/prog.c
@@ -1,10 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "view.h"
+#include <string.h>
 #include "linkedList.h"
+#include "view.h"
 
-int main() 
-{
+void printMenu() {
+    printf("GIF Maker Menu:\n");
+    printf("0. Exit\n");
+    printf("1. Add a new frame\n");
+    printf("2. Remove a frame\n");
+    printf("3. Change the position of a frame\n");
+    printf("4. Change the duration of a frame\n");
+    printf("5. Change the duration of all frames\n");
+    printf("6. Display all frames\n");
+    printf("7. Play the GIF\n");
+}
+
+void addFrame(FrameNode** frameList) {
+    Frame newFrame;
+
+    printf("Enter the path of the image: ");
+    scanf("%s", newFrame.path);
+    printf("Enter the duration (in milliseconds): ");
+    scanf("%d", &newFrame.duration);
+    printf("Enter the name of the frame: ");
+    scanf("%s", newFrame.name);
+
+    // Check if frame name already exists
+    if (findFrame(*frameList, newFrame.name)) {
+        printf("Frame with this name already exists. Please choose a different name.\n");
+        return;
+    }
+
+    // Check if image file exists
+    FILE* file = fopen(newFrame.path, "r");
+    if (!file) {
+        printf("Image file does not exist. Please check the path and try again.\n");
+        return;
+    }
+    fclose(file);
+
+    insertFrame(frameList, newFrame);
+    printf("Frame added successfully.\n");
+}
+
+void removeFrame(FrameNode** frameList) {
+    char name[20];
+    printf("Enter the name of the frame to remove: ");
+    scanf("%s", name);
+
+    if (deleteFrame(frameList, name)) {
+        printf("Frame removed successfully.\n");
+    }
+    else {
+        printf("Frame not found.\n");
+    }
+}
+
+void changeFramePosition(FrameNode** frameList) {
+    char name[20];
+    int newPosition;
+    printf("Enter the name of the frame to move: ");
+    scanf("%s", name);
+    printf("Enter the new position: ");
+    scanf("%d", &newPosition);
+
+    changeFrameOrder(frameList, name, newPosition);
+}
+
+void changeFrameDuration(FrameNode** frameList) {
+    char name[20];
+    int newDuration;
+    printf("Enter the name of the frame to change duration: ");
+    scanf("%s", name);
+    printf("Enter the new duration (in milliseconds): ");
+    scanf("%d", &newDuration);
+
+    changeFrameTime(*frameList, name, newDuration);
+}
+
+void changeAllDurations(FrameNode** frameList) {
+    int newDuration;
+    printf("Enter the new duration for all frames (in milliseconds): ");
+    scanf("%d", &newDuration);
+
+    changeAllFrameTimes(*frameList, newDuration);
+    printf("All frame durations updated successfully.\n");
+}
+
+void printFrames(FrameNode* frameList) {
+    printf("Frames in the GIF:\n");
+    printFramesList(frameList);
+}
+
+int main() {
     FrameNode* frameList = NULL;
     int choice;
 
@@ -13,8 +102,7 @@ int main()
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        switch (choice) 
-        {
+        switch (choice) {
         case 0:
             printf("Exiting the program.\n");
             return 0;
